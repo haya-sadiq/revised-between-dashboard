ebb388d5909b4dce8367d87eb5dd86a3be306710
Haya Sadiq <hayasadiq3@gmail.com>
2026-01-28 19:32:05 +0530

    fix view detail button

--- src/module/dashboard/templates/dashboard.tsx
+++ src/module/dashboard/templates/dashboard.tsx
@@ -3,7 +3,7 @@ import { WorkercardList } from "../components/worker-card-list";
 
 export const DashboardTemplate = () => {
   return (
-    <main className="mx-3 xs:mx-5 flex-1 overflow-y-auto">
+    <main className="mx-2 xs:mx-5 flex-1 overflow-y-auto">
       <BirthworkerTabs />
       <WorkercardList />
     </main>
